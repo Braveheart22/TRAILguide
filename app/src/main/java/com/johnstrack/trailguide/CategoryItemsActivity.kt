620a7f78b981b68a0bc93c58dc9d41e82cd3c369
John Strack <Braveheart22@GMail.com>
2018-03-29 17:30:57 -0400

    Create activities

---
+++ app/src/main/java/com/johnstrack/trailguide/CategoryItemsActivity.kt
@@ -0,0 +1,12 @@
+package com.johnstrack.trailguide
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CategoryItemsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_items)
+    }
+}
