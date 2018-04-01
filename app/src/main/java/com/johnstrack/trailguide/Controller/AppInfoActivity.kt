3874476ce05320e003a052ecd63db7a0c4190794
John <Braveheart22@GMail.com>
2018-04-01 12:22:44 -0400

    Close app info bbutton

--- app/src/main/java/com/johnstrack/trailguide/Controller/AppInfoActivity.kt
+++ app/src/main/java/com/johnstrack/trailguide/Controller/AppInfoActivity.kt
@@ -10,4 +10,8 @@ class AppInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
     }
+
+    fun backToAppClicked () {
+        finish()
+    }
 }
